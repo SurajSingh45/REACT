b9e38b13f2bdb52b2d2195bcb642c9d85eeac796
SurajSingh45 <suraj.12012199@lpu.in>
2023-01-28 16:06:11 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Heading from './components/Heading';
+import Section1 from './components/Section1';
+import Section2 from './components/Section2';
+import Section3 from './components/Section3';
+
+function App() {
+  return <div className="App">
+    <Heading/>
+  </div>
+  
+}
+
+export default App;
